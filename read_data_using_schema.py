610c5b26ceada63de2df002e9f0d6760e7fa8f67
Hussam Elmaghraby <hossam.elmaghraby@yahoo.com>
2021-02-06 18:08:56 +0200

    last commit

--- read_data_using_schema.py
+++ read_data_using_schema.py
@@ -1,4 +1,6 @@
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
+import pyspark.sql.functions as F
 
 fire_schema = StructType([
     StructField("CallNumber", IntegerType(), True),
@@ -33,6 +35,65 @@ fire_schema = StructType([
 
 
 # Use the dataFrameReader ..
-sf_fire_file = "../data/fire-calls.csv"
+sf_fire_file = "../Projects/Reading_from_files_using_schema/data/fire-calls.csv"
 fire_df = spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
 fire_df.show()
+
+# ----------------------------------------------------------------------------
+#                      Saving and writing in many formats
+# Save it as parquet
+parquet_path = "any path"
+fire_df.write.format("parquet").save(parquet_path)
+
+# Save it as Parquet Table
+parquet_table = "any path"
+fire_df.write.format("parquet").saveAsTable(parquet_table
+
+# ----------------------------------------------------------------------------
+#               Performing transformations and actions
+few_frie_df=(fire_df.select("CallNumber", "UnitID", "IncidentNumber").where(
+    col("CallType") != "Medical Incident"))
+
+few_frie_df.show(5, truncate=False)
+
+(fire_df
+.select("CallType").where(col("CallType").isNotNull())
+.alias("DistinctCallTypes").agg(countDistinct("CallType")).show())
+
+# filter for only distinct non-null CallTypes from all the rows
+(fire_df
+.select("CallType")
+.where(col("CallType").isNotNull()).distinct().show(10, False))
+
+
+new_fire_df=fire_df.withColumnRenamed("Delay", "ResponseDelayedinMins")
+(new_fire_df.select("ResponseDelayedinMins").where(
+    col("ResponseDelayedinMins") > 5).show(5, False))
+
+# Convert to more usable formats
+fire_ts_df=(new_fire_df
+.withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy"))
+.drop("CallDate")
+.withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy"))
+.drop("WatchDate")
+.withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"),
+"MM/dd/yyyy hh:mm:ss a"))
+.drop("AvailableDtTm"))
+# Select the converted columns
+fire_ts_df.select("IncidentDate", "OnWatchDate",
+                  "AvailableDtTS").show(5, False)
+fire_ts_df.select(year('IncidentDate')).distinct().orderBy(
+    year("IncidentDate")).show(10, False)
+
+# the most common types of fire calls
+(fire_ts_df.select("CallType")
+.where(col("CallType").isNotNull()).groupBy("CallType")
+.count().orderBy("count", ascending=False)
+.show(n=10, truncate=False))
+
+# some Computatinos
+(fire_ts_df
+.select(F.sum("NumAlarms"), F.avg("ResponseDelayedinMins"),
+F.min("ResponseDelayedinMins"), F.max("ResponseDelayedinMins"))
+.show())
+# ----------------------------------------------------------------------------
--- read_data_using_schema.scala
+++ read_data_using_schema.scala
@@ -1,4 +1,7 @@
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{functions => F}
+
 object Reading_file {
 
 val fireSchema = StructType(
@@ -36,5 +39,47 @@ val fireSchema = StructType(
 val fireCalls = "../data/fire-calls.csv"
 val fireDF = spark.read.schema(fireSchema).option("header" , true).csv(fireCalls)
 fireDF.show()
+
+// ------------------------------------------------------------------------
+//                       Saving and in many formats..
+// Save it as parquet
+val parquetPath = "any path"
+fireDF.write.format("parquet").save(parquetPath)
+
+// Save it as Parquet Table
+val parquetTable = "any path"
+fireDF.write.format("parquet").saveAsTable(parquetTable)
+// ------------------------------------------------------------------------
+//                  Performing transformations and actions
+val fewFireDF = fireDF.select("CallNumber" , "UnitID" , "IncidentNumber").where($"CallType" =!= "Medical Incident")
+fewFireDF.show(5 , false)
+
+//show distinct call types
+fireDF.select("CallType").where(col("CallType").isNotNull).agg(countDistinct('CallType) as 'DistinctCallTypes).show()
+
+//filter for only distinct non-null CallTypes from all the rows
+fireDF.select("CallType").where(col("CallType").isNotNull).distinct().show(10, false)
+
+//rename  Delay column..
+val newFireDF = fireDF.withColumnRenamed("Delay" , "ResposneDelayedinMins")
+
+// select delays longer than 5 
+newFireDF.select("ResposneDelayedinMins").where($"ResposneDelayedinMins" > 10).show()
+
+val fireTsDF = newFireDF.withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy")).drop("CallDate")
+.withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy")).drop("WatchDate")
+.withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"),"MM/dd/yyyy hh:mm:ss a")).drop("AvailableDtTm")
+// Select the converted columns
+fireTsDF.select("IncidentDate", "OnWatchDate", "AvailableDtTS").show()
+fireTsDF.select(year($"IncidentDate")).distinct().orderBy(year($"IncidentDate")).show()
+
+// the most common types of fire calls
+fireTsDF
+.select("CallType")
+.where(col("CallType").isNotNull)
+.groupBy("CallType")
+.count()
+.orderBy(desc("count"))
+.show(10, false)
 
 }
